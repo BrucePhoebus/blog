05c050db359abeed775b9580a2919bb37a4c0744
zhengyekun <1634372267@qq.com>
2018-12-16 16:44:40 +0800

    简单的

--- config/plugin.js
+++ config/plugin.js
@@ -2,3 +2,13 @@
 
 // had enabled by egg
 // exports.static = true;
+
+exports.view = {
+    enable: true,
+    package: 'egg-view'
+};
+
+exports.art = {
+    enable: true,
+    package: 'egg-view-art'
+};
